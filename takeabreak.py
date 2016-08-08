53ac04e73f89718dc09b45576cfc7df529982c58
Shirshandu <shirshandu@yahoo.com>
2016-08-09 02:02:56 +0530

    Take a break program

---
+++ takeabreak.py
@@ -0,0 +1,19 @@
+# SCENARIO
+
+# A programmer spends too much time at a stretch in their computers
+# A Python program which refreshes them to take a break_count
+# In this case i am playing a youtube song after every 2 hours
+# Fell free to fork the project and enhance the code or shoot new ideas
+
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print ("This program started on"+time.ctime())
+
+while (break_count < total_breaks):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=WwliN6p_zWw")
+	break_count = break_count + 1
